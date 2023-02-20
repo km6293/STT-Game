847360bd1b33e4412fdea82573da74cfe3088814
km6293 <alstn7616@naver.com>
2023-02-20 19:25:42 +0900

    tts 게임 기본틀 제작

--- script.js
+++ script.js
@@ -1,114 +1,147 @@
-// TTS (Text To Speech) ============================================
+// game.txt파일에 있는 단어들을 들고온다.
+const txtFilePath = "game.txt"; // txt 파일 경로
+const request = new XMLHttpRequest();
+let vocabularyList = [];
+request.open("GET", txtFilePath, true);
+request.onreadystatechange = function () {
+  if (request.readyState === 4 && request.status === 200) {
+    const fileContent = request.responseText;
+    vocabularyList = fileContent.split(",");
+  }
+};
+request.send();
 
-// 음성 합성 객체
-const synth = window.speechSynthesis;
+// 게임 요소 선택
+const startBtn = document.getElementById("start-btn");
+const gameContainer = document.getElementById("game-container");
+const player = document.getElementById("player");
+const obstacle = document.getElementById("obstacle");
+const scoreDisplay = document.getElementById("score");
+const popup = document.getElementById("popup");
+const finalScoreDisplay = document.getElementById("final-score");
+const yesBtn = document.getElementById("yes-btn");
+const darkModeBtn = document.getElementById("dark-mode-btn");
 
-// 음성 합성 기능을 위한 설정
-const voices = synth.getVoices();
-const voicesOptions = new SpeechSynthesisUtterance();
+// 초기 변수 설정
+let gameStarted = false;
+let score = 0;
+let obstacleSpeed = 1; // 장애물 이동 속도
+let obstacleInterval; // 장애물 이동 setInterval ID
+let recognition; // 음성인식 객체
 
-// 말하기
-function speak(text) {
-  // 선택한 목소리
-  voicesOptions.voice = voices.find((voice) => voice.lang === "ko-KR");
+// 음성인식 지원 여부 확인
+if (!("webkitSpeechRecognition" in window)) {
+  alert("음성인식이 지원되지 않는 브라우저입니다.");
+} else {
+  recognition = new webkitSpeechRecognition();
+  recognition.lang = "ko-KR"; // 언어 설정
+  recognition.continuous = true; // 계속해서 음성인식 수행
+  // 연속적으로 음성 인식 결과를 받아오기 위해 interimResults 속성을 true로 설정.
+  recognition.interimResults = true;
+  recognition.onresult = function (event) {
+    // 난수 생성
+    let randomNumber =
+      Math.floor(Math.random() * (vocabularyList.length - 1)) + 1;
 
-  // 읽는 속도
-  voicesOptions.rate = document.getElementById("rate").value;
+    // const transcript = event.results[event.results.length - 1][0].transcript;
+    // console.log(event.results[event.results.length - 1]);
 
-  // 음 높이
-  voicesOptions.pitch = document.getElementById("pitch").value;
-
-  // 음량
-  voicesOptions.volume = document.getElementById("volume").value;
-
-  // 읽을 Text
-  voicesOptions.text = text;
-
-  synth.speak(voicesOptions);
+    if (event.results[event.results.length - 1].isFinal === false) {
+      playerJump();
+    }
+  }; // 음성인식 시작
+}
+function startRecognition() {
+  recognition.start();
 }
 
-// 플레이 버튼
-const startSpeech = document.getElementById("startSpeech");
-startSpeech.addEventListener("click", () => {
-  const input = document.getElementById("input");
-  speak(input.value);
-});
-
-// 멈춤 버튼
-const stopSpeech = document.getElementById("stopSpeech");
-stopSpeech.addEventListener("click", () => {
-  synth.cancel();
-});
-
-// STT (Speech To Text) ============================================
-
-// 음성 인식 객체 생성
-const recognition = new webkitSpeechRecognition();
-
-// 인식 설정
-recognition.lang = "ko-KR"; // 인식할 언어 설정
-recognition.continuous = true; // 연속적으로 인식할지 여부 설정
-recognition.interimResults = false; // 중간 결과를 출력할지 여부 설정
-
-recognition.onresult = (e) => {
-  const last = e.results.length - 1;
-  const speech = e.results[last][0].transcript;
-  document.getElementById("input").value = speech;
-};
-
-// 음성 인식 시작
-const startText = document.getElementById("startText");
-startText.addEventListener("click", () => {
-  recognition.start();
-});
-
-// 음성 인식 종료
-const stopText = document.getElementById("stopText");
-stopText.addEventListener("click", (event) => {
+// 음성인식 멈춤
+function stopRecognition() {
   recognition.stop();
+}
+
+// 플레이어 점프
+function playerJump() {
+  player.style.bottom = "100px";
+  setTimeout(() => {
+    player.style.bottom = "0px";
+  }, 200);
+}
+
+// 게임 시작
+function startGame() {
+  gameStarted = true;
+  score = 0;
+  scoreDisplay.innerText = "점수: " + score;
+  obstacle.style.right = "-50px";
+  obstacleInterval = setInterval(function () {
+    obstacle.style.right =
+      parseInt(obstacle.style.right) + obstacleSpeed + "px";
+    if (parseInt(obstacle.style.right) >= gameContainer.offsetWidth - 100) {
+      obstacle.style.right = "-50px";
+      score++;
+      scoreDisplay.innerText = "점수: " + score;
+      obstacleSpeed += 0.5;
+    }
+    if (checkCollision(player, obstacle)) {
+      endGame();
+    }
+  }, 10);
+  startRecognition();
+}
+
+// 게임 종료
+function endGame() {
+  stopRecognition();
+  clearInterval(obstacleInterval);
+  gameStarted = false;
+  popup.style.display = "block";
+  finalScoreDisplay.innerText = score;
+}
+
+// 재시작
+function restartGame() {
+  popup.style.display = "none";
+  startGame();
+}
+
+// 충돌 검사
+function checkCollision(player, obstacle) {
+  const playerRect = player.getBoundingClientRect();
+  const obstacleRect = obstacle.getBoundingClientRect();
+  return !(
+    playerRect.bottom < obstacleRect.top ||
+    playerRect.top > obstacleRect.bottom ||
+    playerRect.right < obstacleRect.left ||
+    playerRect.left > obstacleRect.right
+  );
+}
+
+// 이벤트 리스너 추가
+startBtn.addEventListener("click", function () {
+  if (!gameStarted) {
+    startGame();
+  }
 });
 
-// record =====================================
+yesBtn.addEventListener("click", function () {
+  restartGame();
+});
 
-// 오디오 녹음을 위한 미디어 스트림 객체 생성
-// navigator.mediaDevices
-//   .getUserMedia({ audio: true })
-//   .then(function (stream) {
-//     const mediaRecorder = new MediaRecorder(stream);
-//     let chunks = [];
+// 다크모드 on/off 버튼 클릭 시 이벤트 처리
+darkModeBtn.addEventListener("click", function () {
+  const body = document.querySelector("body");
+  const buttons = document.querySelectorAll("button");
+  const gameElements = document.querySelectorAll("#game-container div");
+  const scoreDisplay = document.getElementById("score");
 
-//     // 클릭 이벤트 발생 시 녹음 시작
-//     document.getElementById("startRecord").addEventListener("click", () => {
-//       mediaRecorder.start();
-//     });
+  body.classList.toggle("dark-mode");
+  buttons.forEach((button) => button.classList.toggle("dark-mode"));
+  gameElements.forEach((element) => element.classList.toggle("dark-mode"));
+  scoreDisplay.classList.toggle("dark-mode");
 
-//     // 클릭 이벤트 발생 시 녹음 종료 및 blob 생성
-//     document.getElementById("stopRecord").addEventListener("click", () => {
-//       mediaRecorder.stop();
-//     });
-
-//     // 녹음된 데이터 처리 이벤트 핸들러 등록
-//     mediaRecorder.addEventListener("dataavailable", function (e) {
-//       chunks.push(e.data);
-//     });
-
-//     // 녹음 종료 이벤트 핸들러 등록
-//     mediaRecorder.addEventListener("stop", function () {
-//       // blob 형식으로 녹음된 데이터 생성
-//       const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
-//       console.log("Blob created: ", blob);
-
-//       // 녹음된 오디오를 재생하는 audio 요소 생성
-//       const audio = document.createElement("audio");
-//       audio.controls = true;
-//       const audioUrl = URL.createObjectURL(blob);
-//       audio.src = audioUrl;
-//       document.body.appendChild(audio);
-
-//       // chunks 초기화
-//       chunks = [];
-//     });
-//   })
-//   .catch(function (err) {
-//     console.log("Error:", err);
-//   });
+  const darkModeText = body.classList.contains("dark-mode")
+    ? "다크모드: On"
+    : "다크모드: Off";
+  darkModeBtn.innerText = darkModeText;
+});
